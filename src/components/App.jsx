fd08ec471edcd51803cdf385f6da1e843a1e8118
sanjayvjacob <sanjayvinodjacob@gmail.com>
2024-02-26 22:11:03 +0000

    Added one contact card

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Card from "./Card";
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      <Card />
+      <Card />
+
+    </div>
+  );
+}
+
+export default App;
